972d238f7ab35e6789e8cac4d70adafd8eef4d47
Rodney Puplampu <101155875+puplampuclou@users.noreply.github.com>
2024-02-19 15:19:33 -0600

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+#Google Terraform main.tf provider code.
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.16.0"
+    }
+  }
+}
+provider "google" {
+  # Configuration options
+  project     = "puplampu-inc-project01"
+  region      = "us-central1"
+  zone		  = "us-central-a"
+}
