16ef8c892ab23670055008ccc3983e0574130af8
Rodney Puplampu <101155875+puplampuclou@users.noreply.github.com>
2024-02-19 15:25:23 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,47 @@
-resource "google_storage_bucket" "demo-storage-puplampu-2024" {
-  encryption {
-    default_kms_key_name = "projects/puplampu-inc-project01/locations/us-central1/keyRings/demo-storage-puplampu-2024/cryptoKeys/demo-storage-puplampu-2024"
-  }
-
-  force_destroy               = false
-  location                    = "US-CENTRAL1"
-  name                        = "demo-storage-puplampu-2024"
-  project                     = "puplampu-inc-project01"
-  public_access_prevention    = "inherited"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
+resource "google_kms_key_ring" "key_ring" {
+  name     = "demo-key-timestamp-2024"
+  location = "us-central"  # Replace with your desired location
 }
-# terraform import google_storage_bucket.demo-storage_puplampu_2024 demo-storage-puplampu-2024
 
+resource "google_kms_crypto_key" "crypto_key" {
+  name            = "demo-timestamp-rp-2024"
+  key_ring        = google_kms_key_ring.key_ring.id
+  purpose         = "ENCRYPT_DECRYPT"
+}
 
-resource "google_compute_disk" "lamp_3_vm" {
+# Grant necessary permissions to the Key, using the retrieved bucket's service account
+#resource "google_kms_crypto_key_iam_binding" "storage_bucket_access" {
+#  crypto_key_id = google_kms_crypto_key.crypto_key.id
+#  role          = "roles/storage.objectCreator"
+#  members       = ["serviceAccount:${data.google_storage_bucket.demo_bucket.iam_service_account.selfLink}"]
+#}
+
+# Storage bucket resource with default KMS encryption
+resource "google_storage_bucket" "demo_bucket" {
+  name     = "demo-timestamp-rp-2024"  # Ensure this matches the data resource name
+  location = "us-central"  # Replace with your desired location
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.crypto_key.name
+  }
+}
+
+output "default_kms_key_name" {
+  value = google_kms_crypto_key.crypto_key.name
+}
+
+# Data resource to retrieve information about the existing bucket
+#data "google_storage_bucket" "demo_bucket" {
+#  name = "demo-timestamp-rp-2024"  # Replace with the actual bucket name
+#}
+
+resource "google_compute_disk" "lamp_4_vm" {
   image                     = "https://www.googleapis.com/compute/beta/projects/click-to-deploy-images/global/images/lamp-v20240121"
-  name                      = "lamp-3-vm"
+  name                      = "lamp-4-vm"
   physical_block_size_bytes = 4096
   project                   = "puplampu-inc-project01"
   size                      = 20
   type                      = "pd-standard"
   zone                      = "us-central1-a"
 }
-# terraform import google_compute_disk.lamp_4_vm projects/puplampu-inc-project01/zones/us-central1-a/disks/lamp-3-vm
+# terraform import google_compute_disk.lamp_4_vm projects/puplampu-inc-project01/zones/europe-west4-a/disks/lamp-4-vm
